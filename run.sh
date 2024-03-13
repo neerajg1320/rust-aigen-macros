290e4dfcef3d647fd4766e2f68405809192c8080
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2024-03-13 17:11:25 +0530

    Initial commit

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 cargo new --lib my_proc_macro
 cd my_proc_macro
 
+
+# Add macros in src/lib.rs
+
+# Add [lib] tag in Cargo.toml
+
+# Add dependences
+cargo add syn 
+cargo add quote
+cargo add proc-macro2
+
+# Add full features for syn
+syn={ version="2.0.52", features=["full"] }
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,33 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, ItemFn};
+use syn::{parse_macro_input, DeriveInput, Ident, ItemFn};
 
+// Derive Procedural Macro
+// Macro name is between the braces: HelloWorld
+#[proc_macro_derive(HelloWorld)]
+pub fn helloworld_object_derive(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Name would contain the name of the struct or the enum on which the macro would be applied
+    let name = input.ident;
+
+    // Generate the code to paste into the user's program
+    let expanded = quote! {
+        imple HelloWorld for #name {
+            fn hello_world() {
+                println!("Hello World");
+            }
+        }
+    };
+
+    // Hand the output tokens back to the compile
+    TokenStream::from(expanded)
+}
+
+// Attribute like procedural macro
+// The function name is the macro name
 #[proc_macro_attribute]
 pub fn function_to_string(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the input function
@@ -25,4 +50,24 @@ pub fn function_to_string(_attr: TokenStream, item: TokenStream) -> TokenStream 
     };
 
     output.into()
+}
+
+// Functional like procedural macro
+// The function name is the macro name
+#[proc_macro]
+pub fn create_struct(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as Ident);
+
+    // Construct a string representation of the struct definition
+    let name = &input;
+    let expanded = quote! {
+        struct #name {
+            x: i32,
+            y: i32,
+        }
+    };
+
+    // Return the generated struct as TokenStream
+    expanded.into()
 }
